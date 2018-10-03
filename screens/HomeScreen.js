83ce2bcfe7ad4e99655c3665366b9bbef4496522
Charlie Stewart <cstewart3>
2018-10-02 20:01:49 -0400

    really important change

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -30,9 +30,9 @@ export default class HomeScreen extends React.Component {
               }
               style={styles.welcomeImage}
             />
-			
+
 			<Text style={styles.getStartedText}>
-				You're gonna love this app, trust me. its bad
+				You're gonna love this app, trust me. its badf
 			</Text>
           </View>
 
